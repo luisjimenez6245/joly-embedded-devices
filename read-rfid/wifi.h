4ba293ba1a06ab54b2259b3237e1c3cb07e92243
Luis Jiménez <luisjimenez6245@hotmail.com>
2020-06-14 23:10:02 -0500

    Update wifi.h

--- read-rfid/wifi.h
+++ read-rfid/wifi.h
@@ -2,4 +2,4 @@
 
 const char *ssid = "Mujica";
 const char *password = "Mug2215562.";
-String host = "http://192.168.1.104:8080/joly-api/accessKey/GAefMVZpWO0JnxR4RM1I/?device_name=pla0oWDd8r2yIsxOXjdn&device_token=kSyE5C220EfYqXWydc2w";
+String host = "http://app.data-fin.com/joly-api/accessKey/GAefMVZpWO0JnxR4RM1I/?device_name=pla0oWDd8r2yIsxOXjdn&device_token=kSyE5C220EfYqXWydc2w";
